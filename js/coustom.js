2d59d53ab35a1ce0596b5fa2765017f9de7de1a7
mehedihasanshamim-star <57722840+mehedihasanshamim-star@users.noreply.github.com>
2020-07-04 17:21:43 +0600

    Add files via upload

---
+++ js/coustom.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+	/* -- out- typed js --*/
+	var typed = new Typed('.typing-1', {
+		strings: ["Mehedihasan shamim",
+		],
+		loop:true,
+		backSpeed:100,
+		typeSpeed: 100,
+		backDelay: 900
+	});
+
+});
